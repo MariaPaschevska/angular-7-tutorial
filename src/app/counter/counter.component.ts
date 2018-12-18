57fc237e0a644355279c0c52c754a34bf88ca3e2
Maria Pashchevska <maria.paschevska@gmail.com>
2018-12-18 17:49:03 +0200

    Small fixes

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -15,18 +15,27 @@ export class CounterComponent implements OnInit {
   constructor(private counterService: CounterService) { }
 
   ngOnInit() {
-    let subscription1, subscription2;
+    let subscription1, subscription2, subscription3;
 
-    this.counterService.getCounter().subscribe(count => this.count = count);
-    subscription1 = this.counterService.getSubjectCounter().subscribe(subjectCount => this.subjectCount = subjectCount);
-    subscription2 = this.counterService.getSubjectCounter().subscribe(subjectCount2 => this.subjectCount2 = subjectCount2);
+    subscription1 = this.counterService.getCounter()
+      .subscribe(count => this.count = count);
+
+    subscription2 = this.counterService.getSubjectCounter()
+      .subscribe(subjectCount => this.subjectCount = subjectCount);
+
+    subscription3 = this.counterService.getSubjectCounter()
+      .subscribe(subjectCount2 => this.subjectCount2 = subjectCount2);
 
     setTimeout(() => {
       subscription1.unsubscribe();
-    }, 5000);
+    }, 20000);
 
     setTimeout(() => {
       subscription2.unsubscribe();
+    }, 5000);
+
+    setTimeout(() => {
+      subscription3.unsubscribe();
     }, 10000);
   }
 }
