8136efb5d807aea99bb4c053935c619e476d2848
Maria Pashchevska <maria.paschevska@gmail.com>
2018-12-19 14:24:26 +0200

    counter service injected to app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CounterService} from "./counter.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
+  count: number;
+  subjectCount: number;
+
+  constructor(private counterService: CounterService) { }
+
+  ngOnInit() {
+    let subscription1 = this.counterService.getCounter()
+      .subscribe(count => this.count = count);
+
+    let subscription2 = this.counterService.getSubjectCounter()
+      .subscribe(subjectCount => this.subjectCount = subjectCount);
+
+    setTimeout(() => {
+      subscription1.unsubscribe();
+    }, 20000);
+
+    setTimeout(() => {
+      subscription2.unsubscribe();
+    }, 5000);
+  }
 }
