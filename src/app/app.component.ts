14e6f683b49c678b656babf0ec48c235192cd088
Maria Pashchevska <maria.paschevska@gmail.com>
2018-12-28 13:01:21 +0200

    all services moved out into /Services directory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {CounterService} from "./counter.service";
+import {CounterService} from "./services/counter.service";
 
 @Component({
   selector: 'app-root',
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import {CounterService} from "../counter.service";
+import {CounterService} from "../services/counter.service";
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import {template} from "@angular/core/src/render3";
