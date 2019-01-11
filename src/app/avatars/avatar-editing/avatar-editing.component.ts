f47311dc7c925b1528cd55fe58a9904a706023d0
Maria Pashchevska <maria.paschevska@gmail.com>
2019-01-11 12:16:19 +0200

    Avatar edit simple form added

--- src/app/avatars/avatar-editing/avatar-editing.component.ts
+++ src/app/avatars/avatar-editing/avatar-editing.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {AvatarasService} from "../../services/avataras.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-avatar-editing',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvatarEditingComponent implements OnInit {
 
-  constructor() { }
+  avatar: object;
+
+  avatarEditForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    historyTime: new FormControl(''),
+    country: new FormControl('')
+  });
+
+  constructor(
+    private route: ActivatedRoute,
+    private avatarasService: AvatarasService,
+    private router: Router
+  ) {
+    this.route.params.subscribe(avatar => this.avatar = avatar);
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.avatarEditForm.value);
   }
 
 }
